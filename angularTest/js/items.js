c8ddee73bc94154ea288458b19a17a6dcf77c26b
Denitsa2212 <48585751+Denitsa2212@users.noreply.github.com>
2019-04-14 15:54:38 +0300

    first part

---
+++ angularTest/js/items.js
@@ -0,0 +1,35 @@
+var compost = [
+	"Tea bags",
+	"Fruit",
+	"Pasta noodles",
+	"Egg shells",
+	"Houseplants (not potted)",
+	"Bread",
+	"Rice",
+	"Paper towels & napkins",
+	"Greasy cardboard pizza boxes",
+	"Meat",
+	"Cheese",
+	"Vegetables"
+]
+
+var garbage = [
+	"Packing peanuts",
+	"Fruit stickers",
+	"Ziploc bags",
+	"Foam containers",
+	"Paint cans",
+	"Plastic food containers (dirty)",
+	"Oil & grease",
+	"Incandescent light bulbs"
+]
+
+var recycle = [
+	"Алуминиево фолио",
+	"Стъклени ботилки и буркани",
+	"Кенчета",
+	"Хартия",
+	"Картони и кутий",
+	"Пластична торба",
+	"Пластмасова бутилка"
+]
